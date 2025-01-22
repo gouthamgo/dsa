136ab54c0b7102a1e7c59f47a08499b59c2b16e1
Goutham M <gouthamgo@yahoo.com>
2025-01-22 13:37:00 +1100

    anagram with char count

--- 2J22.py
+++ 2J22.py
@@ -20,8 +20,6 @@ s and t consist of lowercase English letters.
 
 
 
-'''
-
 from collections import Counter
 
 def isValid(s, t):
@@ -29,3 +27,34 @@ def isValid(s, t):
         return False
     return Counter(s) == Counter(t)
 
+
+'''
+
+def isValid(s, t):
+    #  if lengths of strings are not the same 
+    if  len(s) != len(t):
+        return False
+    
+    char_count ={}
+    
+    # count chars in s
+    for char in s:
+        if char in char_count:
+            char_count[char] +=1
+        else:
+            char_count[char]=1
+
+            # decerment the char count and remove if 0
+
+    for char in t:
+        if char not in char_count:
+            return False
+        char_count[char] -=1
+        if char_count[char] == 0:
+            del char_count[char]
+
+    return len(char_count)  == 0
+    
+
+print(isValid("racecar", "carrace"))  # True
+print(isValid("hello", "world"))  # False
