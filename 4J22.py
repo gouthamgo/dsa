d2148e663e8f05a5b19504c45776ae57a37b097d
Goutham M <gouthamgo@yahoo.com>
2025-01-22 16:11:35 +1100

    binary search

--- 4J22.py
+++ 4J22.py
@@ -31,3 +31,23 @@ Constraints:
 
 
 '''
+
+def searchTarget(nums,target):
+
+    left = 0 
+    right=len(nums)-1
+
+    while left <= right:
+
+        middle = (left+right)//2
+
+        if nums[middle] == target:
+            return middle
+        elif target < nums[middle]:
+            right = middle -1 
+        else:
+            left = middle +1 
+            
+    return -1 
+
+
