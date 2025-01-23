d098e96594cb4603f2a392c153b771614c5909d8
Goutham M <gouthamgo@yahoo.com>
2025-01-24 10:12:21 +1100

    linked list learn

---
+++ J24/J24.py
@@ -0,0 +1,36 @@
+'''
+Reverse Linked List
+Given the beginning of a singly linked list head, reverse the list, and return the new beginning of the list.
+
+Example 1:
+
+Input: head = [0,1,2,3]
+
+Output: [3,2,1,0]
+Example 2:
+
+Input: head = []
+
+Output: []
+Constraints:
+
+0 <= The length of the list <= 1000.
+-1000 <= Node.val <= 1000
+
+
+'''
+
+# def reverseList(nums):
+#     stack=[]
+
+#     for num in nums:
+#         stack.append(num)
+    
+#     return stack[::-1]
+
+
+# print(reverseList([0,1,2,3]))
+    
+
+
+
