e5805dab7bb69439a7b85c653357fae7acf10c22
Goutham M <gouthamgo@yahoo.com>
2025-03-06 13:51:13 +1100

    typescript

---
+++ M06/src/app.js
@@ -0,0 +1,5 @@
+"use strict";
+let numbers = [1, 2, 3, 4];
+// let varName: <type> = value 
+let mixed = [1, 'ra', 23, true];
+numbers.push('seven');
---
+++ M06/src/app.ts
@@ -0,0 +1,7 @@
+let numbers: number[] = [1,2,3,4]
+
+// let varName: <type> = value 
+
+let mixed: any[] = [1, 'ra', 23, true]
+
+numbers.push('seven')
