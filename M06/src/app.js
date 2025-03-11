18d1502b727934d59aa257bd27b15a79e8654e28
Goutham M <gouthamgo@yahoo.com>
2025-03-12 10:54:30 +1100

    transfomers sentiment analysis

--- M06/src/app.js
+++ M06/src/app.js
@@ -2,4 +2,18 @@
 let numbers = [1, 2, 3, 4];
 // let varName: <type> = value 
 let mixed = [1, 'ra', 23, true];
-numbers.push('seven');
+// numbers.push('seven') -- not allowed 
+/**
+ * Tuple
+ 
+ */
+let personTuple = ['check', 30, true];
+/**
+ * Objects
+ */
+let person;
+person = {
+    name: 'Alice',
+    location: 'USA',
+    isPlayer: true
+};
--- M06/src/app.ts
+++ M06/src/app.ts
@@ -4,4 +4,47 @@ let numbers: number[] = [1,2,3,4]
 
 let mixed: any[] = [1, 'ra', 23, true]
 
-numbers.push('seven')
+// numbers.push('seven') -- not allowed 
+
+/**
+ * Tuple 
+ 
+ */
+
+let personTuple: [string, number, boolean] = ['check', 30 , true]
+
+
+/**
+ * Objects
+ */
+
+
+let person : {
+    name: string,
+    location: string,
+    isPlayer: boolean;
+};
+
+person={
+    name : 'Alice',
+    location:'USA',
+    isPlayer: true
+}
+
+
+// person.isPlayer = 'Yes' - not allowed 
+
+// cant add new object without all properties 
+
+// person={
+//     name: 'te',
+//     location: 'Den'
+// }
+
+
+interface Person{
+    name : String[] = 
+}
+
+
+
