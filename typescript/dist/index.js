bc3d0f62ec3d2107d0a85d5ffee753c7dcf4646a
TS14\Goutham <Goutham.Masavarapu@takingshape.com>
2025-03-26 16:03:48 +1100

    adding habit tracker

--- typescript/dist/index.js
+++ typescript/dist/index.js
@@ -1,16 +1,1 @@
 "use strict";
-let message = "Hello, TypeScript!";
-console.log(message);
-console.log(3);
-let ok = "lets test";
-console.log(ok);
-console.log(2);
-let trail;
-trail = true;
-let yest = 'yes';
-let identifier;
-identifier = 123;
-let numbers = [1, 2, 3];
-let books = [
-    { title: 'yes', author: 'not ok' }
-];
