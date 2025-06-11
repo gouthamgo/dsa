aed0d3f46da4e576e03ac409bd2b19d018399201
TS14\Goutham <Goutham.Masavarapu@takingshape.com>
2025-06-11 13:52:30 +1000

    some basic py

---
+++ dsa/J11.py
@@ -0,0 +1,39 @@
+my_variale = 10
+another_variable='python'
+
+is_active = True
+
+def check():
+    '''this is a function'''
+    print('Testing to pass the argument')
+
+def getOutput(num):
+    return my_variale * num
+
+check()
+sum = getOutput(2)
+print(f"The check is : {sum}")
+
+
+garments= ["petite", "swimwear","checks"]
+
+
+catgeories={
+    "newin": "Latest",
+    "total": 300,
+    "locale":"AU"
+}
+
+
+for key in catgeories:
+    print(f"Key : {key}")
+
+
+for value in catgeories.values():
+    print(f"Vlaue: {value}")
+
+for key,value in catgeories.items():
+    print(f"{key} : {value}")
+
+for item in garments:
+    print(f"{item}")
