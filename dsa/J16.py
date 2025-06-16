3548a808598fcb1fe734b44a246a1426e4a727dc
Goutham M <gouthamgo@yahoo.com>
2025-06-16 15:49:07 +1000

    basic

---
+++ dsa/J16.py
@@ -0,0 +1,29 @@
+# Variables 
+
+size = 10
+length = 1.5
+
+product_name="Petite"
+
+is_active= True
+check = None 
+
+# if size>10:
+#     print("greater >9")
+# elif size == 5:
+#     print("exact 5")
+# else:
+#     print("lastone")
+
+
+# for i in range(3):
+#     print(i)
+
+# for i in range(5):
+#     if i ==2:
+#         continue
+#     if i ==4:
+#         break
+#     print(i)
+
+
