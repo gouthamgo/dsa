d8e4503df13a0fd50eb2f583f79b3d519c6219f9
Goutham M <gouthamgo@yahoo.com>
2025-07-02 09:19:18 +1000

    question and check yaml

---
+++ dsa/questions/longest-substring.py
@@ -0,0 +1,7 @@
+'''
+Longest Substring Without Repeating Characters
+
+Description: Given a string, find the length of the longest substring without repeating characters.
+
+Example: Input: "abcabcbb" Output: 3 ("abc")
+'''
