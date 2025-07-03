d7d75bd7d940f34c5dbe85f8dffb3c08953ce77c
TS14\Goutham <Goutham.Masavarapu@takingshape.com>
2025-07-03 14:14:50 +1000

    longest substring

--- dsa/questions/longest-substring.py
+++ dsa/questions/longest-substring.py
@@ -5,3 +5,25 @@ Description: Given a string, find the length of the longest substring without re
 
 Example: Input: "abcabcbb" Output: 3 ("abc")
 '''
+
+
+def longest_substring(s:str):
+    char_set = set()
+    # i initialised an empty set 
+    left = 0
+    max_length = 0 
+    #  initalisation on left side and to keep track of length found 
+
+    for right in range(len(s)):
+        while s[right] in char_set:
+            char_set.remove(s[left])
+            left += 1
+        char_set.add(s[right])
+
+
+        max_length = max(max_length,right-left +1)
+    return max_length
+
+
+
+
