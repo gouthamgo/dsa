5652e1964eb86de8c9dad55be1ad8d2661485038
Goutham M <gouthamgo@yahoo.com>
2025-08-04 09:22:58 +1000

    new problems

---
+++ prac/triplet-sum.py
@@ -0,0 +1,36 @@
+'''
+Triplet Sum
+Difficulty - Medium
+Given an array of integers, return all triplets [a, b, c] such that a + b + c = 0 . The solution must not contain duplicate triplets (e.g., [1, 2, 3] and [2, 3, 1] are considered duplicates). If no such triplets are found, return an empty array.
+
+Each triplet can be arranged in any order, and the output can be returned in any order.
+
+Example:
+Input: nums = [0, -1, 2, -3, 1]
+Output: [[-3, 1, 2], [-1, 0, 1]]
+
+
+we get an array of nums --> 
+
+'''
+
+def find_sum(nums):
+    s = nums.sort()
+    print(s)
+
+    # sort the numbers 
+
+
+
+
+    return []
+
+
+
+
+
+
+
+print(find_sum([0, -1, 2, -3, 1]))
+
+
